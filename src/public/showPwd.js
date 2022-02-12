d5e604ba82dcbd4674a63f4daad423047c5072bb
pablosgreccia <pablosgreccia@gmail.com>
2022-02-12 16:27:16 -0300

    add regExp to validate passwords

--- src/public/showPwd.js
+++ src/public/showPwd.js
@@ -1,3 +1,5 @@
+
+
 document.getElementById('show-pwd-img').addEventListener('mouseover', ()=>{
     document.getElementById('pwd').type = 'text';
     document.getElementById('confirmPwd').type = 'text';
@@ -16,14 +18,28 @@ document.getElementById('pwd').addEventListener('input', ()=>{
 document.getElementById('confirmPwd').addEventListener('input', ()=>{
     hiddenCheckAttribute();
 })
+hiddenCheckAttribute();
+
 function hiddenCheckAttribute(){
     console.log('hola')
     const cpwdContent = document.getElementById('confirmPwd').value;
     const pwdContent = document.getElementById('pwd').value;
-    if(cpwdContent == pwdContent){
-        document.getElementById('correct-pwd').hidden = false;
+    if((cpwdContent == pwdContent) && (pwdContent.length > 0) && (containsSpecialChars(pwdContent))){
+        document.getElementById('correct-pwd').className = 'bi bi-check-all';
+        document.getElementById('correct-pwd').title = 'Both Passwords Match'
     }
     else{
-        document.getElementById('correct-pwd').hidden = true;
+        document.getElementById('correct-pwd').className = 'bi bi-info-circle';
+        document.getElementById('correct-pwd').title = 'The password must contain at leats 5 characters with one special character'
+    }
+}
+
+function containsSpecialChars(password){
+    var strongRegex = new RegExp("^(?=.*[.,?!@#\$%\^&\*])(?=.{5,})");
+
+    if(strongRegex.test(password)) {
+        return true;
+    } else{
+        return false;
     }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -13,17 +13,35 @@ router.post('/users/signup', (req, res) => {
     //validar q el mail no exista
     //validar que contrasenia sea segura
 
-    const {name, email, pwd, confirmpwd} = req.body;
-    if((name == "") || (email == "") || (pwd == "") || (confirmpwd == "")){
+    const {name, email, pwd, confirmPwd} = req.body;
+    if((name == "") || (email == "") || (pwd == "") || (confirmPwd == "")){
         let message = "You must fill all fields to create a user.";
-        res.render("users/signup", {name, email, pwd, confirmpwd, message});
+        res.render("users/signup", {name, email, pwd, confirmPwd, message});
     }
-    else if(pwd != confirmpwd){
+    else if(pwd != confirmPwd){
         let message = "Passwords doesn't match.";
-        res.render("users/signup", {name, email, pwd, confirmpwd, message});
+        res.render("users/signup", {name, email, pwd, confirmPwd, message});
+    }
+    else if(!containsSpecialChars(pwd)){
+        //console.log(pwd);
+        let message = 'The password must contain at leats 5 characters with one special character';
+        res.render("users/signup", {name, email, pwd, confirmPwd, message});
     }
     else{
         res.send('ok');
+    }
+
+    function containsSpecialChars(password){
+        //var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+        var strongRegex = new RegExp("^(?=.*[.,?!@#\$%\^&\*])(?=.{5,})");
+
+        if(strongRegex.test(password)) {
+            console.log('verdadero');
+            return true;
+        } else{
+            console.log('falso');
+            return false;
+        }
     }
 });
 
